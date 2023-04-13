db53cc50fc333a187e95385ea25f8e604deaf031
SampreetG22 <128352957+SampreetG22@users.noreply.github.com>
2023-04-13 14:09:39 +0530

    Add files via upload

---
+++ components/EachItem/index.js
@@ -0,0 +1,31 @@
+import './index.css'
+
+const EachHistoryItem = props => {
+  const {historyDetails, deleteHistory} = props
+  const {id, timeAccessed, logoUrl, title, domainUrl} = historyDetails
+
+  const onDeleteClick = () => {
+    deleteHistory(id)
+  }
+  return (
+    <li className="listItem">
+      <div className="listItemContainer">
+        <p className="timeCSS">{timeAccessed}</p>
+        <div className="imageTitleUrlContainer">
+          <img src={logoUrl} alt="instagram" className="logoAlter" />
+          <p className="titleCSS">{title}</p>
+          <p className="urlCSS">{domainUrl}</p>
+        </div>
+        <button onClick={onDeleteClick} type="button" className="deleteBtn">
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/delete-img.png"
+            alt="delete"
+            className="deleteCSS"
+          />
+        </button>
+      </div>
+    </li>
+  )
+}
+
+export default EachHistoryItem
